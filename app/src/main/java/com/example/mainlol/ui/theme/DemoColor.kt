68a979ce41c8c74e386709d0310ca2d3ecbace50
mainlol <sim.caceres@duocuc.cl>
2025-09-22 09:18:39 -0300

    lol

---
+++ app/src/main/java/com/example/mainlol/ui/theme/DemoColor.kt
@@ -0,0 +1,10 @@
+package com.example.mainlol.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val Greer_80 = Color(0xFF00913F)
+
+val Grrer_90= Color(0xFFF54927)
+
+val ver_39=Color(0xFF57C7C3)
